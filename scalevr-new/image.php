543343b758c1cf49f2be7fadd191639f5e08935a
rethenya <61034898+rethenya@users.noreply.github.com>
2020-02-27 16:07:09 +0530

    Update image.php

--- scalevr-new/image.php
+++ scalevr-new/image.php
@@ -18,7 +18,7 @@
     // loop through the array of files and print them all in a list
 		for($index=0; $index < $indexCount; $index++) {
 			$extension = substr($dirArray[$index], -3);
-			if ( $extension =='jfif' or $extension =='jpg'){ // list only jpgs
+			if (  $extension =='jpg'){ // list only jpgs
 				echo '
   
 			<a target="_blank" href="DisplayProduct.html?database/' . $dirArray[$index] . '">
