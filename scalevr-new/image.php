dc4991d72378545c36e0111ab8773e80e5fa26fb
rethenya <61034898+rethenya@users.noreply.github.com>
2020-04-20 11:16:47 +0530

    Add files via upload

--- scalevr-new/image.php
+++ scalevr-new/image.php
@@ -18,15 +18,13 @@
     // loop through the array of files and print them all in a list
 		for($index=0; $index < $indexCount; $index++) {
 			$extension = substr($dirArray[$index], -3);
-			if (  $extension =='jpg'){ // list only jpgs
+			if ( $extension =='jfif' or $extension =='jpg'){ // list only jpgs
 				echo '
   
 			<a target="_blank" href="DisplayProduct.html?database/' . $dirArray[$index] . '">
                
 			  <img id="image" style="border: 1px solid #ddd; border-radius: 4px; padding: 5px;width: 150px;height:150px;"  src="database/' . $dirArray[$index] . '" alt="Image" />
-			 </a>
-			 
-			  ';
+			 </a>';
 			 
               
 			}	
